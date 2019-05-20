1b03f77827be5e96b71a420af24908ca2de7f2bd
Nick Shvelidze <captain@pirrate.me>
2019-05-21 01:09:24 +0400

    Set up Nuxt

---
+++ components/Navbar.vue
@@ -0,0 +1,75 @@
+<template>
+  <nav class="navbar is-success is-spaced site-navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">Food4.ge</a>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        :class="{ 'is-active': showNav }"
+        :aria-expanded="String(showNav)"
+        @click="toggleNav()"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div class="navbar-menu" :class="{ 'is-active': showNav }">
+      <div class="navbar-start">
+        <a class="navbar-item">Home</a>
+
+        <a class="navbar-item">Documentation</a>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">More</a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item">About</a>
+            <a class="navbar-item">Jobs</a>
+            <a class="navbar-item">Contact</a>
+            <hr class="navbar-divider">
+            <a class="navbar-item">Report an issue</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">Log in</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+.site-navbar.site-navbar.site-navbar {
+    background-color: #7CB342;
+}
+.site-navbar .navbar-brand {
+    flex-grow: 1;
+}
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            showNav: false
+        }
+    },
+    methods: {
+        toggleNav() {
+            this.showNav = !this.showNav
+        }
+    }
+}
+</script>
