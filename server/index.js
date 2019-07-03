72b9fd5187d12220080d90fb6f8723b0bcb644be
Nick Shvelidze <captain@pirrate.me>
2019-07-04 02:05:04 +0400

    Passport auth

--- server/index.js
+++ server/index.js
@@ -1,40 +1,62 @@
-const express = require('express')
-const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
-const app = express()
-const api = require('./api')
+const express = require('express'),
+  session = require('express-session'),
+  passport = require('./passport'),
+  consola = require('consola'),
+  { Nuxt, Builder } = require('nuxt'),
+  app = express(),
+  api = require('./api');
 
 // Import and Set Nuxt.js options
-let config = require('../nuxt.config.js')
-config.dev = !(process.env.NODE_ENV === 'production')
+let config = require('../nuxt.config.js');
+config.dev = !(process.env.NODE_ENV === 'production');
 
 async function start() {
   // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+  const nuxt = new Nuxt(config);
 
-  const { host, port } = nuxt.options.server
+  const { host, port } = nuxt.options.server;
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.set('trust proxy', 1); // trust first proxy (nginx)
+  app.use(session({
+    secret: 'ajapsandali',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: !config.dev }
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-  app.use('/api', api)
+  app.post('/login', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+      if (err) { return res.status(500).json(err); }
+      if (!user) { return res.status(401).json(info.message); }
+      req.logIn(user, function (err) {
+        if (err) { return res.status(500).json(err); }
+        return res.json(user);
+      });
+    })(req, res, next);
+  });
+
+  app.use('/api', api);
 
   // Build only in dev mode
   if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
+    const builder = new Builder(nuxt);
+    await builder.build();
   } else {
-    await nuxt.ready()
+    await nuxt.ready();
   }
 
   // Give nuxt middleware to express
-  app.use(nuxt.render)
+  app.use(nuxt.render);
 
   // Listen the server
-  app.listen(port, host)
+  app.listen(port, host);
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
-  })
+  });
 }
-start()
+start();
