d5c72c0eda36f024240ed2529437c7fb5477ab8c
Nick Shvelidze <captain@pirrate.me>
2019-07-05 01:45:59 +0400

    API Auth

--- server/api.js
+++ server/api.js
@@ -2,4 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+router.use('/admin', (req, res, next) => {
+    if(!req.user || req.user.role !== 'admin')
+        return res.status(401).send("Unauthorized");
+});
+
+router.use('/user', (req, res, next) => {
+    if(!req.user)
+        return res.status(401).send("Unauthorized");
+    next();
+});
+
 module.exports = router;
